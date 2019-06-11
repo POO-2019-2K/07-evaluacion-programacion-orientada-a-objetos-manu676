79334ea94d9344e357369d68d56d72b74f22f54d
MAU <42820040+manu676@users.noreply.github.com>
2019-06-11 18:33:19 -0500

    main
    fin

--- main.js
+++ main.js
@@ -2,53 +2,53 @@ import Homework from "./homework.js";
 import RegisterH from "./registerH.js";
 
 class Main {
-constructor() {
-let agendaHmw = new RegisterH(
-    document.querySelector("#agenda"),
-    document.querySelector("#info")
-);
+    constructor() {
+        let agendaHmw = new RegisterH(
+            document.querySelector("#agenda"),
+            document.querySelector("#info")
+        );
 
-document.querySelector("#btnAdd").addEventListener("click", () => {
-    let form = document.querySelector("#form");
+        document.querySelector("#btnAdd").addEventListener("click", () => {
+            let form = document.querySelector("#form");
 
-    if (form.checkValidity() === true) {
+            if (form.checkValidity() === true) {
 
-    let name = document.querySelector("#name").value;
-    let theme = document.querySelector("#theme").value;
-    let dateHand = document.querySelector("#dateHandIt").value;
-    dateHand = dateHand.split("-");
+                let name = document.querySelector("#name").value;
+                let theme = document.querySelector("#theme").value;
+                let dateHand = document.querySelector("#dateHandIt").value;
+                dateHand = dateHand.split("-");
 
-    let dateHandIt = new Date(dateHand[0], dateHand[1] - 1, dateHand[2]);
+                let dateHandIt = new Date(dateHand[0], dateHand[1] - 1, dateHand[2]);
 
-    let notes = document.querySelector("#notes").value;
+                let notes = document.querySelector("#notes").value;
 
-    let objHomeworks = {
-        name: name,
-        theme : theme,
-        dateHandIt: dateHandIt,
-        notes: notes,
-    };
+                let objHomeworks = {
+                    name: name,
+                    theme: theme,
+                    dateHandIt: dateHandIt,
+                    notes: notes,
+                };
 
-    let homework = new Homework(objHomeworks);
+                let homework = new Homework(objHomeworks);
 
-    agendaHmw.addHomeworkToDo(homework);
+                agendaHmw.addHomeworkToDo(homework);
+            }
+
+            form.classList.add("was-validated");
+        });
+        let selector = document.getElementById("order");
+        selector.addEventListener("change", () => {
+            let selections = selector.value;
+            if (selections === "Basic") {
+                swal.fire({
+                    type: "error",
+                    title: "error",
+                    text: "Choose an Option, THANK U :3"
+                });
+            } else {
+                agendaHmw._findOrder(selections);
+            }
+        })
     }
-
-    form.classList.add("was-validated"); 
-});
-let selector = document.getElementById("order");
-selector.addEventListener("change",()=>{
-    let selections = selector.value;
-    if (selections === "Basic"){
-        swal.fire({
-            type: "error",
-            title: "error",
-            text : "Choose an Option, THANK U :3"
-            });
-    }else{
-        agendaHmw._findOrder(selections);
-    }
-})
-}
 }
 let mainContactos = new Main();
